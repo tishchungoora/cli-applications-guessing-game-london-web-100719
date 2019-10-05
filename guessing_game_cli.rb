1033e278a3531bd6ef779c991253ddeb8131632c
Tish Chungoora <tishal82@gmail.com>
2019-10-05 20:00:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,13 +1,13 @@
 def run_guessing_game()
   r_num = rand(6) + 1
-  input = gets
+  puts ""
+  input = gets.chomp
   
   if input.to_i == r_num
     puts "You guessed the correct number!"
   elsif input.to_i != r_num
     puts "Sorry! The computer guessed #{r_num}."
   elsif input == "exit"
-    puts 
-end
-  
+    puts "Goodbye!"
+  end
 end

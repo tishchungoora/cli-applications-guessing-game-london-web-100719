89e5123dcea0b225295ce3ec9b135c2bc6c0185d
Tish Chungoora <tishal82@gmail.com>
2019-10-05 20:06:27 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,11 +4,11 @@ def run_guessing_game()
   
   case input
     when input.to_i == r_num
-    puts "You guessed the correct number!"
-  elsif input.to_i != r_num
-    puts "Sorry! The computer guessed #{r_num}."
-  elsif input == "exit"
-    puts "Goodbye!"
-  else
+      puts "You guessed the correct number!"
+    when input.to_i != r_num
+      puts "Sorry! The computer guessed #{r_num}."
+    when input == "exit"
+      puts "Goodbye!"
+    else
   end
 end

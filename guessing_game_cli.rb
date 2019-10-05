a25897f20ab4ca6b0b23657b4f7c6f67ad7b1a52
Tish Chungoora <tishal82@gmail.com>
2019-10-05 20:05:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,8 @@ def run_guessing_game()
   r_num = rand(6) + 1
   input = gets.chomp
   
-  if input.to_i == r_num
+  case input
+    when input.to_i == r_num
     puts "You guessed the correct number!"
   elsif input.to_i != r_num
     puts "Sorry! The computer guessed #{r_num}."
